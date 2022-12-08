f785cf97a5f23ac61e71c386a96178bff6d58441
yeray <yeray.cabello@codurance.com>
2022-12-08 13:10:56 +0100

    elevation

--- Primer/Primer/Primer/Program.cs
+++ Primer/Primer/Primer/Program.cs
@@ -1,3 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using DeFuncto;
+using DeFuncto.Extensions;
 
-Console.WriteLine("Hello, World!");
+public record Fruit(int Weight);
+
+public enum Errors
+{
+    Unauthorized,
+    NotFound
+}
+
+[Flags]
+public enum Roles
+{
+    None = 0,
+    AccessFruits = 1
+}
+
+public static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var output =
+            await GetFruitWeight(10, 8)
+                .Match(
+                    w => $"Weight is: {w}",
+                    e =>
+                        e switch
+                        {
+                            Errors.Unauthorized => "Unauthorized",
+                            Errors.NotFound => "Not found"
+                        }
+                );
+        
+        Console.WriteLine(output);
+        return 0;
+    }
+
+    public static async Task<string> GetFileContents(string fileName)
+    {
+        var lines = await File.ReadAllLinesAsync(fileName);
+        return string.Concat(lines);
+    }
+
+    public static AsyncResult<int, Errors> GetFruitWeight(int userID, int fruitID) =>
+        from roles in Ok<Roles, Errors>(GetRoles(userID)).Async()
+        from _ in roles.VerifyRole(Roles.AccessFruits).Async()
+        from fruit in FindFruit(fruitID).Result(Errors.NotFound)
+        select fruit.Weight;
+
+    public static Result<Unit, Errors> VerifyRole(this Roles self, Roles role) =>
+        self.HasFlag(role) ? Ok(unit) : Error(Errors.Unauthorized);
+
+    public static Roles GetRoles(int id) =>
+        id == 10 ? Roles.AccessFruits : Roles.None;
+
+    public static AsyncOption<Fruit> FindFruit(int id) =>
+        id == 8 ? Some(new Fruit(13)) : None;
+}
+
+public class NotFoundException : Exception { }
+public class UnauthorizedException : Exception { }
